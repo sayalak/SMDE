e92e319ac3b36a746d10a77e6651c0ac9ade3702
sayalak <sayala24@hotmail.com>
2025-03-04 19:27:26 -0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const section = document.querySelector(link.getAttribute('href'));
+        section.scrollIntoView({ behavior: 'smooth' });
+    });
+});
